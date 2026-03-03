5d12cc4e9332869ae81a9ce6ea1efbf42bd65479
詹卓佳 <1300757947@qq.com>
2026-03-03 23:27:43 +0800

    路由配置

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-  <el-button type="primary">Primary</el-button>
+  <!-- 一级组件出口 -->
+  <RouterView />
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+//createRouter：创建router实例对象
+//createWebHistory：创建history模式的路由
+import login from "@/views/login/login-index.vue";
+import layout from "@/views/layout/layout-index.vue";
+import home from "@/views/home/home-index.vue";
+import category from "@/views/category/category-index.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: "/",
+      component: layout,
+      children: [
+        {
+          path: "",
+          component: home,
+        },
+        {
+          path: "category",
+          component: category,
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/login",
+      component: login,
     },
   ],
-})
+});
 
-export default router
+export default router;
