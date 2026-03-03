e4865bc82e91ed841b94294208faccad41f6c9ee
詹卓佳 <1300757947@qq.com>
2026-03-03 17:09:32 +0800

    axios的基础封装

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
+//测试接口函数
+import { getCategory } from "@/apis/testAPI";
+getCategory().then((res) => {
+  console.log(res);
+});
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(router);
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
