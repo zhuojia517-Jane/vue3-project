41247b8e2b308d80621d8c1d1fe227b46e0a1cee
詹卓佳 <1300757947@qq.com>
2026-03-04 21:38:20 +0800

    优化pinia请求

---
+++ src/stores/category.js
@@ -0,0 +1,15 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import { getCategoryAPI } from "@/apis/layout";
+export const useCategoryStore = defineStore("category", () => {
+// state 导航列表数据
+  const categoryList = ref([])
+
+  // action 获取导航数据的方法
+  const getCategory = async () => {
+    const res = await getCategoryAPI()
+    categoryList.value = res.result
+  }
+
+  return { categoryList, getCategory };
+});
