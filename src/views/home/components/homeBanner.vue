a7994e1e6830cccfc6c0ce4222218e992c49bc6a
詹卓佳 <1300757947@qq.com>
2026-03-05 11:31:38 +0800

    home-Banner轮播图

--- src/views/home/components/homeBanner.vue
+++ src/views/home/components/homeBanner.vue
@@ -1,4 +1,43 @@
-<template>
-  我是banner
+<script setup>
+import { getBannerApi } from '@/apis/home'
+import { onMounted, ref } from 'vue'
 
+const bannerList = ref([])
+
+const getBanner = async () => {
+  const res = await getBannerApi()
+  console.log(res)
+  bannerList.value = res.result
+}
+
+onMounted(() => getBanner())
+
+</script>
+
+
+
+<template>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+
+
+<style scoped lang='scss'>
+.home-banner {
+  max-width: 1240px;
+  width: 100%;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+}
+</style>
