a11d309dd4927b93e969f19e4239af70603ce27b
詹卓佳 <1300757947@qq.com>
2026-03-08 19:33:30 +0800

    二级分类-面包屑导航实现

--- src/views/subCategory/sub-index.vue
+++ src/views/subCategory/sub-index.vue
@@ -1,6 +1,15 @@
 <script setup>
 
-
+import { getCategoryFilterAPI } from "@/apis/category.js"
+import { ref, onMounted } from "vue"
+import { useRoute } from "vue-router"
+const filterData = ref({})
+const route = useRoute()
+const getCategoryFilter = async () => {
+  const res = await getCategoryFilterAPI(route.params.id)
+  filterData.value = res.result
+}
+onMounted(() => getCategoryFilter())
 </script>
 
 <template>
@@ -9,9 +18,9 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家
+        <el-breadcrumb-item :to="{ path: `/category/${filterData.parentId}` }">{{ filterData.parentName }}
         </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ filterData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
