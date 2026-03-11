3013124dda4ae9d0e882bf344656858b38c1562b
詹卓佳 <1300757947@qq.com>
2026-03-11 17:24:33 +0800

    结算-切换地址-切换地址功能

--- src/views/checkout/index.vue
+++ src/views/checkout/index.vue
@@ -4,7 +4,8 @@ import {ref,onMounted,computed} from 'vue'
 import {getCheckoutInfoAPI} from '@/apis/checkout'
 const checkInfo = ref({})  // 订单对象
 const curAddress = ref({})  // 地址对象
-const showDialog=ref(false)
+const showDialog=ref(false) //是否展示切换地址弹框
+const activeAddress=ref({})  // 选中的地址对象
 const getCheckoutInfo=async ()=>{
     const res =await getCheckoutInfoAPI()
     checkInfo.value=res.result
@@ -15,6 +16,13 @@ onMounted( ()=>{
     getCheckoutInfo()
 })
 
+const confirm=()=>{
+    showDialog.value=false
+    curAddress.value=activeAddress.value
+}
+const switchAddress=(item)=>{
+activeAddress.value=item
+}
 
 </script>
 
@@ -35,7 +43,7 @@ onMounted( ()=>{
               </ul>
             </div>
             <div class="action">
-              <el-button size="large" @click="showDialog = true">切换地址</el-button>
+              <el-button size="large" @click="showDialog = true" >切换地址</el-button>
               <el-button size="large" @click="addFlag = true">添加地址</el-button>
             </div>
           </div>
@@ -118,7 +126,8 @@ onMounted( ()=>{
   <!-- 切换地址 -->
    <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
   <div class="addressWrapper">
-    <div class="text item" v-for="item in checkInfo.userAddresses"  :key="item.id">
+    <div class="text item" :class="{ active: activeAddress.id === item.id }" @click="switchAddress(item)"
+        v-for="item in checkInfo.userAddresses" :key="item.id">
       <ul>
       <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
       <li><span>联系方式：</span>{{ item.contact }}</li>
@@ -128,8 +137,8 @@ onMounted( ()=>{
   </div>
   <template #footer>
     <span class="dialog-footer">
-      <el-button>取消</el-button>
-      <el-button type="primary" >确定</el-button>
+      <el-button @click="showDialog = false">取消</el-button>
+      <el-button type="primary" @click="confirm">确定</el-button>
     </span>
   </template>
 </el-dialog>
